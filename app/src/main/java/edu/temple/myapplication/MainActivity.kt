0105ec19f8fb20abcde89a119debabce7b7d7294
BennyQian1 <tuq28313@temple.edu>
2025-04-10 11:41:48 -0400

    -Timer -Added Menu

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -9,6 +9,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 
@@ -59,5 +61,29 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.stopButton).setOnClickListener {
             timerBinder?.stop();
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_start -> {
+                timerBinder?.run{
+                    timerBinder?.start(10) ?: run {
+
+                    }
+                }
+            }
+            R.id.action_stop -> {
+                timerBinder?.stop()
+            }
+            else -> {
+                return false
+            }
+        }
+        return true
     }
 }
